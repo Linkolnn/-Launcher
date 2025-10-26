499de3d495c5e98e50c9d8598fe7a52081bd4618
Linkolnn <rozarioa1974@gmail.com>
2025-10-26 23:36:39 +0500

    ver 2.2 реалищован функционал полной блокировки экрана

--- app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/service/SensorControlService.java
+++ app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/service/SensorControlService.java
@@ -1,6 +1,6 @@
 package com.customlauncher.app.service;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000J\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0010\u000b\n\u0002\b\u0005\u0018\u0000 \u001d2\u00020\u0001:\u0001\u001dB\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u000b\u001a\u00020\fH\u0002J\b\u0010\r\u001a\u00020\fH\u0002J\u0014\u0010\u000e\u001a\u0004\u0018\u00010\u000f2\b\u0010\u0010\u001a\u0004\u0018\u00010\u0011H\u0016J\b\u0010\u0012\u001a\u00020\fH\u0016J\b\u0010\u0013\u001a\u00020\fH\u0016J\"\u0010\u0014\u001a\u00020\u00152\b\u0010\u0010\u001a\u0004\u0018\u00010\u00112\u0006\u0010\u0016\u001a\u00020\u00152\u0006\u0010\u0017\u001a\u00020\u0015H\u0016J\b\u0010\u0018\u001a\u00020\u0019H\u0002J\b\u0010\u001a\u001a\u00020\u0019H\u0002J\b\u0010\u001b\u001a\u00020\u0019H\u0002J\b\u0010\u001c\u001a\u00020\u0019H\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000R\u0012\u0010\t\u001a\u00060\nR\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001e"}, d2 = {"Lcom/customlauncher/app/service/SensorControlService;", "Landroid/app/Service;", "()V", "componentName", "Landroid/content/ComponentName;", "devicePolicyManager", "Landroid/app/admin/DevicePolicyManager;", "powerManager", "Landroid/os/PowerManager;", "wakeLock", "Landroid/os/PowerManager$WakeLock;", "disableTouchSensor", "", "enableTouchSensor", "onBind", "Landroid/os/IBinder;", "intent", "Landroid/content/Intent;", "onCreate", "onDestroy", "onStartCommand", "", "flags", "startId", "tryDisableTouchViaRoot", "", "tryDisableTouchViaSettings", "tryEnableTouchViaRoot", "tryEnableTouchViaSettings", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0010\u000b\n\u0002\b\u0003\u0018\u0000 \u00132\u00020\u0001:\u0001\u0013B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0002J\b\u0010\u0005\u001a\u00020\u0004H\u0002J\u0014\u0010\u0006\u001a\u0004\u0018\u00010\u00072\b\u0010\b\u001a\u0004\u0018\u00010\tH\u0016J\b\u0010\n\u001a\u00020\u0004H\u0016J\b\u0010\u000b\u001a\u00020\u0004H\u0016J\"\u0010\f\u001a\u00020\r2\b\u0010\b\u001a\u0004\u0018\u00010\t2\u0006\u0010\u000e\u001a\u00020\r2\u0006\u0010\u000f\u001a\u00020\rH\u0016J\b\u0010\u0010\u001a\u00020\u0011H\u0002J\b\u0010\u0012\u001a\u00020\u0011H\u0002\u00a8\u0006\u0014"}, d2 = {"Lcom/customlauncher/app/service/SensorControlService;", "Landroid/app/Service;", "()V", "disableTouchSensor", "", "enableTouchSensor", "onBind", "Landroid/os/IBinder;", "intent", "Landroid/content/Intent;", "onCreate", "onDestroy", "onStartCommand", "", "flags", "startId", "tryDisableTouchViaSettings", "", "tryEnableTouchViaSettings", "Companion", "app_debug"})
 public final class SensorControlService extends android.app.Service {
     @org.jetbrains.annotations.NotNull
     private static final java.lang.String TAG = "SensorControlService";
@@ -9,10 +9,6 @@ public final class SensorControlService extends android.app.Service {
     @org.jetbrains.annotations.NotNull
     public static final java.lang.String ACTION_ENABLE_SENSOR = "com.customlauncher.app.ENABLE_SENSOR";
     private static boolean isDisabled = false;
-    private android.os.PowerManager powerManager;
-    private android.os.PowerManager.WakeLock wakeLock;
-    private android.app.admin.DevicePolicyManager devicePolicyManager;
-    private android.content.ComponentName componentName;
     @org.jetbrains.annotations.NotNull
     public static final com.customlauncher.app.service.SensorControlService.Companion Companion = null;
     
@@ -41,14 +37,6 @@ public final class SensorControlService extends android.app.Service {
     }
     
     private final void enableTouchSensor() {
-    }
-    
-    private final boolean tryDisableTouchViaRoot() {
-        return false;
-    }
-    
-    private final boolean tryEnableTouchViaRoot() {
-        return false;
     }
     
     private final boolean tryDisableTouchViaSettings() {
--- app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/ui/MainActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/ui/MainActivity.java
@@ -1,8 +1,7 @@
 package com.customlauncher.app.ui;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000^\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\n\u0018\u0000 02\u00020\u0001:\u00010B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0019\u001a\u00020\u001aH\u0002J\b\u0010\u001b\u001a\u00020\u001aH\u0002J\b\u0010\u001c\u001a\u00020\u000eH\u0002J\b\u0010\u001d\u001a\u00020\u001aH\u0002J\b\u0010\u001e\u001a\u00020\u001aH\u0016J\u0012\u0010\u001f\u001a\u00020\u001a2\b\u0010 \u001a\u0004\u0018\u00010!H\u0014J\b\u0010\"\u001a\u00020\u001aH\u0014J\u001a\u0010#\u001a\u00020\u000e2\u0006\u0010$\u001a\u00020%2\b\u0010&\u001a\u0004\u0018\u00010\'H\u0016J\u001a\u0010(\u001a\u00020\u000e2\u0006\u0010$\u001a\u00020%2\b\u0010&\u001a\u0004\u0018\u00010\'H\u0016J\b\u0010)\u001a\u00020\u001aH\u0014J\b\u0010*\u001a\u00020\u001aH\u0002J\b\u0010+\u001a\u00020\u001aH\u0002J\b\u0010,\u001a\u00020\u001aH\u0002J\u0010\u0010-\u001a\u00020\u001a2\u0006\u0010.\u001a\u00020\u000eH\u0002J\b\u0010/\u001a\u00020\u001aH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u000b\u001a\u0004\u0018\u00010\fX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u000f\u001a\u00020\u00108BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0013\u0010\u0014\u001a\u0004\b\u0011\u0010\u0012R\u000e\u0010\u0015\u001a\u00020\u0016X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0017\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0018\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u00061"}, d2 = {"Lcom/customlauncher/app/ui/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "appAdapter", "Lcom/customlauncher/app/ui/adapter/AppGridAdapter;", "binding", "Lcom/customlauncher/app/databinding/ActivityMainBinding;", "keyCombinationReceiver", "Landroid/content/BroadcastReceiver;", "keyPressHandler", "Landroid/os/Handler;", "longPressRunnable", "Ljava/lang/Runnable;", "powerPressed", "", "preferences", "Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "getPreferences", "()Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "preferences$delegate", "Lkotlin/Lazy;", "viewModel", "Lcom/customlauncher/app/ui/viewmodel/AppViewModel;", "volumeDownPressed", "volumeUpPressed", "checkDeviceAdminPermission", "", "checkOverlayPermission", "isAccessibilityServiceEnabled", "observeViewModel", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onDestroy", "onKeyDown", "keyCode", "", "event", "Landroid/view/KeyEvent;", "onKeyUp", "onResume", "setupListeners", "setupRecyclerView", "toggleAppsVisibility", "toggleDoNotDisturb", "enable", "updateVisibility", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000^\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\t\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0019\u001a\u00020\u001aH\u0002J\b\u0010\u001b\u001a\u00020\u000eH\u0002J\b\u0010\u001c\u001a\u00020\u001aH\u0002J\b\u0010\u001d\u001a\u00020\u001aH\u0016J\u0012\u0010\u001e\u001a\u00020\u001a2\b\u0010\u001f\u001a\u0004\u0018\u00010 H\u0014J\b\u0010!\u001a\u00020\u001aH\u0014J\u001a\u0010\"\u001a\u00020\u000e2\u0006\u0010#\u001a\u00020$2\b\u0010%\u001a\u0004\u0018\u00010&H\u0016J\u001a\u0010\'\u001a\u00020\u000e2\u0006\u0010#\u001a\u00020$2\b\u0010%\u001a\u0004\u0018\u00010&H\u0016J\b\u0010(\u001a\u00020\u001aH\u0014J\b\u0010)\u001a\u00020\u001aH\u0002J\b\u0010*\u001a\u00020\u001aH\u0002J\b\u0010+\u001a\u00020\u001aH\u0002J\u0010\u0010,\u001a\u00020\u001a2\u0006\u0010-\u001a\u00020\u000eH\u0002J\b\u0010.\u001a\u00020\u001aH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u000b\u001a\u0004\u0018\u00010\fX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u000f\u001a\u00020\u00108BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0013\u0010\u0014\u001a\u0004\b\u0011\u0010\u0012R\u000e\u0010\u0015\u001a\u00020\u0016X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0017\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0018\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006/"}, d2 = {"Lcom/customlauncher/app/ui/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "appAdapter", "Lcom/customlauncher/app/ui/adapter/AppGridAdapter;", "binding", "Lcom/customlauncher/app/databinding/ActivityMainBinding;", "keyCombinationReceiver", "Landroid/content/BroadcastReceiver;", "keyPressHandler", "Landroid/os/Handler;", "longPressRunnable", "Ljava/lang/Runnable;", "powerPressed", "", "preferences", "Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "getPreferences", "()Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "preferences$delegate", "Lkotlin/Lazy;", "viewModel", "Lcom/customlauncher/app/ui/viewmodel/AppViewModel;", "volumeDownPressed", "volumeUpPressed", "checkOverlayPermission", "", "isAccessibilityServiceEnabled", "observeViewModel", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onDestroy", "onKeyDown", "keyCode", "", "event", "Landroid/view/KeyEvent;", "onKeyUp", "onResume", "setupListeners", "setupRecyclerView", "toggleAppsVisibility", "toggleDoNotDisturb", "enable", "updateVisibility", "app_debug"})
 public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
-    private static final int REQUEST_CODE_DEVICE_ADMIN = 1001;
     private com.customlauncher.app.databinding.ActivityMainBinding binding;
     private com.customlauncher.app.ui.viewmodel.AppViewModel viewModel;
     private com.customlauncher.app.ui.adapter.AppGridAdapter appAdapter;
@@ -17,8 +16,6 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
     private java.lang.Runnable longPressRunnable;
     @org.jetbrains.annotations.NotNull
     private final android.content.BroadcastReceiver keyCombinationReceiver = null;
-    @org.jetbrains.annotations.NotNull
-    public static final com.customlauncher.app.ui.MainActivity.Companion Companion = null;
     
     public MainActivity() {
         super();
@@ -74,22 +71,11 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
         return false;
     }
     
-    private final void checkDeviceAdminPermission() {
-    }
-    
     @java.lang.Override
     protected void onDestroy() {
     }
     
     @java.lang.Override
     public void onBackPressed() {
-    }
-    
-    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0005"}, d2 = {"Lcom/customlauncher/app/ui/MainActivity$Companion;", "", "()V", "REQUEST_CODE_DEVICE_ADMIN", "", "app_debug"})
-    public static final class Companion {
-        
-        private Companion() {
-            super();
-        }
     }
 }
