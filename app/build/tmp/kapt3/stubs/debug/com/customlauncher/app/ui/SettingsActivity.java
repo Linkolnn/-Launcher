65f7763ed7efad697440d9b35cbac0608a7d31e7
Linkolnn <rozarioa1974@gmail.com>
2025-10-29 01:28:59 +0500

    ver 3.3 Добавления возмодности вкл/выкл функционал лаунчера, переработка UI, добавлена возможность использовать стороние паки иконок

--- app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/ui/SettingsActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/customlauncher/app/ui/SettingsActivity.java
@@ -1,14 +1,17 @@
 package com.customlauncher.app.ui;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\r\u0018\u0000 ,2\u00020\u0001:\u0001,B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u000b\u001a\u00020\fJ\b\u0010\r\u001a\u00020\u000eH\u0002J\b\u0010\u000f\u001a\u00020\u000eH\u0002J\u0010\u0010\u0010\u001a\u00020\u000e2\u0006\u0010\u0011\u001a\u00020\u0012H\u0002J\u0010\u0010\u0013\u001a\u00020\u00122\u0006\u0010\u0014\u001a\u00020\u0015H\u0002J\b\u0010\u0016\u001a\u00020\fH\u0002J\b\u0010\u0017\u001a\u00020\fH\u0002J\"\u0010\u0018\u001a\u00020\u000e2\u0006\u0010\u0019\u001a\u00020\u00152\u0006\u0010\u001a\u001a\u00020\u00152\b\u0010\u001b\u001a\u0004\u0018\u00010\u001cH\u0014J\b\u0010\u001d\u001a\u00020\u000eH\u0016J\u0012\u0010\u001e\u001a\u00020\u000e2\b\u0010\u001f\u001a\u0004\u0018\u00010 H\u0014J\b\u0010!\u001a\u00020\u000eH\u0014J\b\u0010\"\u001a\u00020\u000eH\u0002J\b\u0010#\u001a\u00020\u000eH\u0002J\b\u0010$\u001a\u00020\u000eH\u0002J\b\u0010%\u001a\u00020\u000eH\u0002J\b\u0010&\u001a\u00020\u000eH\u0002J\b\u0010\'\u001a\u00020\u000eH\u0002J\b\u0010(\u001a\u00020\u000eH\u0002J\b\u0010)\u001a\u00020\u000eH\u0002J\b\u0010*\u001a\u00020\u000eH\u0002J\b\u0010+\u001a\u00020\u000eH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0005\u001a\u00020\u00068BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\t\u0010\n\u001a\u0004\b\u0007\u0010\b\u00a8\u0006-"}, d2 = {"Lcom/customlauncher/app/ui/SettingsActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "binding", "Lcom/customlauncher/app/databinding/ActivitySettingsBinding;", "preferences", "Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "getPreferences", "()Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "preferences$delegate", "Lkotlin/Lazy;", "areAllPermissionsGranted", "", "checkAllPermissions", "", "checkOverlayPermission", "displayCustomKeys", "keysString", "", "getKeyName", "keyCode", "", "isAccessibilityServiceEnabled", "isDefaultLauncher", "onActivityResult", "requestCode", "resultCode", "data", "Landroid/content/Intent;", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onResume", "setupAppVersion", "setupCustomKeyCombination", "setupGridSelection", "setupHomeScreenSelector", "setupPermissionsSection", "setupTabs", "showHomeScreenDialog", "showRecordKeysDialog", "updateStatusText", "updateUI", "Companion", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000V\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u000b\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0007\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u000f\u0018\u0000 42\u00020\u0001:\u00014B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\u0011\u001a\u00020\u0010J\b\u0010\u0012\u001a\u00020\u0010H\u0002J\b\u0010\u0013\u001a\u00020\u0014H\u0002J\u0010\u0010\u0015\u001a\u00020\u00142\u0006\u0010\u0016\u001a\u00020\u0017H\u0002J\u0010\u0010\u0018\u001a\u00020\u00172\u0006\u0010\u0019\u001a\u00020\u001aH\u0002J\b\u0010\u001b\u001a\u00020\u0010H\u0002J\b\u0010\u001c\u001a\u00020\u0010H\u0002J\b\u0010\u001d\u001a\u00020\u0014H\u0002J\"\u0010\u001e\u001a\u00020\u00142\u0006\u0010\u001f\u001a\u00020\u001a2\u0006\u0010 \u001a\u00020\u001a2\b\u0010!\u001a\u0004\u0018\u00010\"H\u0014J\b\u0010#\u001a\u00020\u0014H\u0016J\u0012\u0010$\u001a\u00020\u00142\b\u0010%\u001a\u0004\u0018\u00010&H\u0014J\b\u0010\'\u001a\u00020\u0014H\u0014J\b\u0010(\u001a\u00020\u0014H\u0002J\b\u0010)\u001a\u00020\u0014H\u0002J\b\u0010*\u001a\u00020\u0014H\u0002J\b\u0010+\u001a\u00020\u0014H\u0002J\b\u0010,\u001a\u00020\u0014H\u0002J\b\u0010-\u001a\u00020\u0014H\u0002J\b\u0010.\u001a\u00020\u0014H\u0002J\b\u0010/\u001a\u00020\u0014H\u0002J\b\u00100\u001a\u00020\u0014H\u0002J\b\u00101\u001a\u00020\u0014H\u0002J\b\u00102\u001a\u00020\u0014H\u0002J\b\u00103\u001a\u00020\u0014H\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000R\u001b\u0010\t\u001a\u00020\n8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\r\u0010\u000e\u001a\u0004\b\u000b\u0010\fR\u000e\u0010\u000f\u001a\u00020\u0010X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u00065"}, d2 = {"Lcom/customlauncher/app/ui/SettingsActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "binding", "Lcom/customlauncher/app/databinding/ActivitySettingsBinding;", "iconPackAdapter", "Lcom/customlauncher/app/ui/adapter/IconPackAdapter;", "iconPackManager", "Lcom/customlauncher/app/utils/IconPackManager;", "preferences", "Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "getPreferences", "()Lcom/customlauncher/app/data/preferences/LauncherPreferences;", "preferences$delegate", "Lkotlin/Lazy;", "shouldScrollToPermissions", "", "areAllPermissionsGranted", "checkAllPermissions", "checkOverlayPermission", "", "displayCustomKeys", "keysString", "", "getKeyName", "keyCode", "", "isAccessibilityServiceEnabled", "isDefaultLauncher", "loadIconPacks", "onActivityResult", "requestCode", "resultCode", "data", "Landroid/content/Intent;", "onBackPressed", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onResume", "scheduleScrollToPermissions", "scrollToPermissions", "setupAppVersion", "setupCustomKeyCombination", "setupFeatureSwitches", "setupGridSelection", "setupHomeScreenSelector", "setupIconPacks", "setupPermissionsSection", "setupTabs", "showHomeScreenDialog", "showRecordKeysDialog", "Companion", "app_debug"})
 public final class SettingsActivity extends androidx.appcompat.app.AppCompatActivity {
     private com.customlauncher.app.databinding.ActivitySettingsBinding binding;
     @org.jetbrains.annotations.NotNull
     private final kotlin.Lazy preferences$delegate = null;
+    private com.customlauncher.app.ui.adapter.IconPackAdapter iconPackAdapter;
+    private com.customlauncher.app.utils.IconPackManager iconPackManager;
     private static final int REQUEST_CODE_WRITE_SETTINGS = 1002;
     private static final int REQUEST_CODE_ACCESSIBILITY = 1003;
     @org.jetbrains.annotations.NotNull
     private static final java.lang.String TAG = "SettingsActivity";
+    private boolean shouldScrollToPermissions = false;
     @org.jetbrains.annotations.NotNull
     public static final com.customlauncher.app.ui.SettingsActivity.Companion Companion = null;
     
@@ -44,13 +47,16 @@ public final class SettingsActivity extends androidx.appcompat.app.AppCompatActi
     private final void setupGridSelection() {
     }
     
+    private final void setupFeatureSwitches() {
+    }
+    
+    private final void setupIconPacks() {
+    }
+    
+    private final void loadIconPacks() {
+    }
+    
     private final void setupHomeScreenSelector() {
-    }
-    
-    private final void updateUI() {
-    }
-    
-    private final void updateStatusText() {
     }
     
     private final void showHomeScreenDialog() {
@@ -66,7 +72,8 @@ public final class SettingsActivity extends androidx.appcompat.app.AppCompatActi
     private final void setupPermissionsSection() {
     }
     
-    private final void checkAllPermissions() {
+    private final boolean checkAllPermissions() {
+        return false;
     }
     
     private final boolean isAccessibilityServiceEnabled() {
@@ -91,6 +98,12 @@ public final class SettingsActivity extends androidx.appcompat.app.AppCompatActi
     
     @java.lang.Override
     protected void onResume() {
+    }
+    
+    private final void scheduleScrollToPermissions() {
+    }
+    
+    private final void scrollToPermissions() {
     }
     
     @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0007X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\b"}, d2 = {"Lcom/customlauncher/app/ui/SettingsActivity$Companion;", "", "()V", "REQUEST_CODE_ACCESSIBILITY", "", "REQUEST_CODE_WRITE_SETTINGS", "TAG", "", "app_debug"})
